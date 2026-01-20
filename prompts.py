1f31e56e6cc32ba5dfd657746ecd8e423935cbd8
trimpbizkit <mgilmour.se@gmail.com>
2026-01-20 15:31:56 -0500

    improved user prompt, tested with fixing a basic bug in calculator app

--- prompts.py
+++ prompts.py
@@ -10,5 +10,7 @@ When a user asks a question or makes a request, make a function call plan. You c
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 
-After any function calls, the contents of the final response should include a summary in the form of a bulleted list, where code is concerned, lead the bullet with relevant function signatures and/or class definitions. 
+After any function calls, the contents of the final response should include a summary in the form of a bulleted list.
+The summary should fit to the context of the prompt, so if prompted to fix a bug in code, summarize the bug and the solution to it.
+However, whenever the contents of a bullet describes a function or class, always lead that bullet with the function signature and/or class definition. 
 """
